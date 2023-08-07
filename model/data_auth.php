a242bcdcdb3245ff9003fb3b0cb911e08fe596cd
Aryo w <aryoyusuf313@gmail.com>
2023-08-07 13:46:04 +0700

    Update SRC Model Authentikasi

--- model/data_auth.php
+++ model/data_auth.php
@@ -75,33 +75,25 @@
 
 	function login(){
 		global $con;
-
 		if (empty($_POST['email']) || empty($_POST['password'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "Email / Password tidak boleh kosong!");
 			return false;
 		}
-
 		$email = mysqli_real_escape_string($con, $_POST['email']);
 		$password = mysqli_real_escape_string($con, $_POST['password']);
-
 		$check_email = "SELECT * FROM users WHERE email = '$email'";
 		$result = mysqli_query($con, $check_email);
-
 		#Cek Email jika sudah terdaftar
 		if (mysqli_num_rows($result) == 0) {
 			$_SESSION['alert'] = warningAlert("Warning! ", "Your email ($email) is not registered");
 			return false;
 		}
-
 		if (mysqli_num_rows($result) > 0) {
-
 			#Cek Password
 			$row = mysqli_fetch_array($result);
 			if (password_verify($password, $row['password'])) {
-
 				#Cek Status
 				if ($row['status'] == 'verified') {
-
 					#Cek Ban
 					if ($row['fail'] > 2) {
 						$_SESSION['alert'] = dangerAlert("Danger!", "Your account is being banned, please contact admin");
@@ -111,7 +103,6 @@
 						$result = mysqli_query($con, $sql);
 						$_SESSION['email'] = $email;
 						$acces = $row['acces'];
-
 						#Cek Hak Akses
 						if ($acces == 1) {
 							$_SESSION['email'] = $email;
@@ -119,14 +110,12 @@
 							alert('Selamat Datang $email');
 								window.location = '../admin/'
 						</script>";
-
 						}elseif ($acces == 2) {
 							$_SESSION['email'] = $email;
 							echo "<script type=text/javascript>
 							alert('Selamat Datang $email');
 								window.location = '../supplier/'
 						</script>";
-
 						}else{
 							$_SESSION['email'] = $email;
 							echo "<script type=text/javascript>
@@ -134,15 +123,12 @@
 								window.location = '../customer/'
 						</script>";
 						}
-
 					}
-
 				}else{
 					$_SESSION['email'] = $email;
 					$_SESSION['alert'] = warningAlert("Warning! ", "It's look like you haven't still verify your email - $email");
 					header('location: user-otp.php');
 				}
-
 			}else{
 				$sql = "UPDATE users SET fail = fail + 1 WHERE email = '$email'";
 				$result = mysqli_query($con, $sql);
@@ -150,7 +136,6 @@
 				$result2 = mysqli_query($con, $sql2);
 				$row = mysqli_fetch_array($result2,MYSQLI_ASSOC);
 				$fail = $row['fail'];
-
 				#Cek Kesalahan Password
 				if($fail > 2){
 					$_SESSION['alert'] = dangerAlert("Danger! ", "Your email has been blocked, please contact the administrator");
@@ -160,28 +145,23 @@
 				return false;
 			}
 		}
-
 	}
 
 	function signup(){
 		global $con;
-
 		if (empty($_POST['username']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['cpassword'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "Form tidak boleh kosong!");
 			return false;
 		}
-
 		$name = mysqli_real_escape_string($con, $_POST['username']);
 		$email = mysqli_real_escape_string($con, $_POST['email']);
 		$password = mysqli_real_escape_string($con, $_POST['password']);
 		$cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
-
 		//Cek username tidak boleh ada space dan spesial karakter
 		if (!preg_match("/^[a-zA-Z0-9_]*$/", $name)) {
 			$_SESSION['alert'] = warningAlert("Warning! ", "Username should not contain special characters!");
 			return false;
 		}
-
 		//Cek kesamaan Username
 		$username_check = "SELECT username FROM users WHERE username = '$name'";
 		$result_username = mysqli_query ($con, $username_check);
@@ -189,13 +169,11 @@
 			$_SESSION['alert'] = infoAlert("Info! ", "Username is alredy exist!");
 			return false;
 		}
-
 		#Cek Password
 		if($password !== $cpassword){
 			$_SESSION['alert'] = infoAlert("Info! ", "Confirm password not matched!");
 			return false;
 		}
-
 		#Cek Email
 		$email_check = "SELECT * FROM users WHERE email = '$email'";
 		$res = mysqli_query($con, $email_check);
@@ -203,7 +181,6 @@
 			$_SESSION['alert'] = infoAlert("Info! ", "Email is already exist!");
 			return false;
 		}
-
 		#ID UNIQ
 		$unik = false;
 		while (!$unik){
@@ -215,7 +192,6 @@
 				$unik = true;
 			}
 		}
-
 		$encpass = password_hash($password, PASSWORD_BCRYPT);
 		$code = rand(999999, 111111);
 		$status = "not verified";
@@ -231,15 +207,6 @@
 			$_SESSION['password'] = $password;
 			header('location: user-otp.php');
 			exit();
-//            if(mail($email, $subject, $message, $sender)){
-//                $_SESSION['alert'] = successAlert("Succes! ", "We've sent a verification code to your email - $email");
-//                $_SESSION['email'] = $email;
-//                $_SESSION['password'] = $password;
-//                header('location: user-otp.php');
-//                exit();
-//            }else{
-//                $_SESSION['alert'] = dangerAlert("Danger! ", "Failed while sending code!");
-//            }
 		}else{
 			$_SESSION['alert'] = dangerAlert("Danger! ", "Failed while inserting data into database!");
 		}
@@ -247,17 +214,14 @@
 
 	function forgot_password(){
 		global $con;
-
 		if (empty($_POST['email'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "Email tidak boleh kosong!");
 			return false;
 		}
-
 		$email = mysqli_real_escape_string($con, $_POST['email']);
 		var_dump($email);
 		$check_email = "SELECT * FROM users WHERE email = '$email'";
 		$run_sql = mysqli_query($con, $check_email);
-
 		#Cek Email
 		if(mysqli_num_rows($run_sql) > 0){
 			$code = rand(999999, 111111);
@@ -267,19 +231,10 @@
 			if($run_query){
 				$subject = "Password Reset Code";
 				$message = "Your password reset code is $code";
-//                $sender = "From: mailerphp000@gmail.com";
 				send_email($email, $subject, $message);
 				$_SESSION['email'] = $email;
 				header('location: reset-code.php');
 				exit();
-//                if(mail($email, $subject, $message, $sender)){
-//                    $_SESSION['email'] = $email;
-//                    $_SESSION['alert'] = infoAlert("Info! ", "We've sent a password reset otp to your email - $email");
-//                    header('location: reset-code.php');
-//                    exit();
-//                }else{
-//                    $_SESSION['alert'] = warningAlert("Warning! ", "Failed while sending code!");
-//                }
 			}else{
 				$_SESSION['alert'] = warningAlert("Warning! ", "Something went wrong!");
 			}
@@ -290,12 +245,10 @@
 
 	function check_otp(){
 		global $con;
-
 		if (empty($_POST['otp'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "OTP code tidak boleh kosong!");
 			return false;
 		}
-
 		$_SESSION['info'] = "";
 		$otp_code = mysqli_real_escape_string($con, $_POST['otp']);
 		$check_code = "SELECT * FROM users WHERE code = $otp_code";
@@ -322,12 +275,10 @@
 
 	function reset_code_otp(){
 		global $con;
-
 		if (empty($_POST['otp'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "OTP code tidak boleh kosong!");
 			return false;
 		}
-
 		$otp_code = mysqli_real_escape_string($con, $_POST['otp']);
 		$check_code = "SELECT * FROM users WHERE code = '$otp_code'";
 		$code_res = mysqli_query($con, $check_code);
@@ -346,12 +297,10 @@
 
 	function change_password(){
 		global $con;
-
 		if (empty($_POST['password']) || empty($_POST['cpassword'])){
 			$_SESSION['alert'] = warningAlert("Warning! ", "Password code tidak boleh kosong!");
 			return false;
 		}
-
 		$password = mysqli_real_escape_string($con, $_POST['password']);
 		$cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
 		if($password !== $cpassword){
